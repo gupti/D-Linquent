a1f7c807636d7fc14b3567f0e63b044f297dbb5f
gupti <paulgupti@gmail.com>
2016-09-29 08:20:33 -0400

    More code overhaul

--- d-linquent.py
+++ d-linquent.py
@@ -1,14 +1,21 @@
+# Main program file
+
 # Parse arguments
-import argparse
+import argparse, sys, urllib, http.client
 
 argParser = argparse.ArgumentParser()
 
 # Options
+
+# Required argument
+argParser.add_argument("host", help="Host IP or address of the extender")
+
 argParser.add_argument("-r", "--reboot",
 	help = "reboot the extender",
 	action = "store_true")
 
+
 args = argParser.parse_args()
 
 if args.reboot:
-	print("Rebootin'")
+	print("Rebooting")
